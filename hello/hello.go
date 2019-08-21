570c0454e40e08738abce74ae00850e9dafde528
leiming <2008weileiming@gmail.com>
2019-08-21 18:31:22 +0800

    commit

--- hello/hello.go
+++ hello/hello.go
@@ -3,14 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World", ""))
 }
 
-const helloPrefix = "Hello, "
+const spanish = "Spanish"
+const french = "French"
+const englishPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return helloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishPrefix
+	}
+	return
 }
