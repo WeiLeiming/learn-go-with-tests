6155842bc58ae2fe30362131dbff7bbde37ee073
leiming <2008weileiming@gmail.com>
2019-10-18 10:26:18 +0800

    Interface

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,6 +13,14 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
 		got := rectangle.Area()
