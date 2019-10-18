399cbe8165b66d23b36625f971b9e3bcd605c59d
leiming <2008weileiming@gmail.com>
2019-10-18 18:01:54 +0800

    Update

--- pointer/wallet_test.go
+++ pointer/wallet_test.go
@@ -3,12 +3,25 @@ package pointer
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	want := Bitcoin(10)
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{Bitcoin(20)}
+		wallet.Withdraw(10)
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 }
